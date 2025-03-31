8f18e0f0b7b2c43dc47cabbc06c8a4655b608998
Zigrik <wssa34@yandex.ru>
2025-03-31 21:10:33 +0300

    second-commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
+func main() {
+	logger := log.New(os.Stdout, "server: ", log.LstdFlags)
+	srv := server.StartServer(logger)
+	if err := srv.HTTPServer.ListenAndServe(); err != nil {
+		logger.Fatal("Error while server start: ", err)
+	}
 }
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,43 @@
 package service
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+// checks whether the text consists only of dots and dashes.
+func checkMorse(text string) bool {
+	arr := []rune(text)
+	for _, v := range arr {
+		if v != '.' && v != '-' && v != ' ' {
+			return false
+		}
+	}
+	return true
+}
+
+// checks for the use of valid characters for Morse code conversion.
+func checkToMorse(text string) bool {
+	text = strings.ToUpper(text)
+	text = strings.ReplaceAll(text, " ", "")
+	arr := []rune(text)
+	for _, v := range arr {
+		_, ok := morse.DefaultMorse[v]
+		if !ok {
+			return false
+		}
+	}
+	return true
+}
+
+// CheckMorseAndConvert converts Morse to text if the input consists of dots and dashes. Otherwise, it converts the input string to Morse code.
+func СheckMorseAndConvert(text string) (string, error) {
+	if checkMorse(text) {
+		return morse.ToText(text), nil
+	} else if checkToMorse(text) {
+		return morse.ToMorse(text), nil
+	}
+	return "", fmt.Errorf("the file cannot be converted. Invalid characters")
+}
